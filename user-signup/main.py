10299f36eecc7e133f289199f0576070a29db32a
Katrinka Swenson <kjswenso@gmail.com>
2019-10-11 09:59:30 -0400

    Added add validation checks

--- user-signup/main.py
+++ user-signup/main.py
@@ -1,16 +1,83 @@
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, render_template
 import cgi
 import os
-import jinja2
-
-template_dir = os.path.join(os.path.dirname(__file__), 'templates')
-jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
 @app.route("/")
 def index():
-    template = jinja_env.get_template('input.html')
-    return template.render()
+    return render_template('input.html')
 
+def isEmpty(user_input):
+    if not user_input:
+        return True
+    else:
+        return False
+
+def hasSpace(user_input):
+    if " " in user_input:
+        return True
+    else:
+        return False
+
+def invalid_length(user_input):
+    if len(user_input) < 3 or len(user_input) > 20:
+        return True
+    else:
+        return False
+
+def valid_email(user_input):
+    if user_input.count('@') == 1 and user_input.count('.') == 1:
+        return True
+    else: 
+        return False
+
+def password_match(pass1, pass2):
+    if pass1 == pass2:
+        return True
+    else: return False
+
+
+@app.route("/", methods=['POST'])
+def validate_input():
+    username = request.form['username']
+    password = request.form['password-init']
+    password_verify = request.form['password-verify']
+    email = request.form['email']
+
+    username_invalid = ""
+    password_invalid = ""
+    email_invalid = ""
+ 
+    #username validation
+    if isEmpty(username):
+        username_invalid = "Please enter a username"
+    elif hasSpace(username):
+        username_invalid = "Username must contain only alphanumeric and special characters"
+    elif invalid_length(username):
+        username_invalid = "Username must be between 3 and 20 characters"
+
+    #password validation
+    if isEmpty(password) or isEmpty(password_verify):
+        password_invalid = "Please enter a password"
+    elif hasSpace(password):
+        password_invalid = "Password must contain only alphanumeric and special characters"
+    elif invalid_length(password):
+        password_invalid = "Password must be between 3 and 20 characters"
+    elif not password_match(password, password_verify):
+        password_invalid = "Passwords do not match. Please reenter"
+    
+
+    #email validation
+    if hasSpace(email):
+        email_invalid = "email must contain only alphanumeric and special characters"
+    elif invalid_length(email):
+        email_invalid = "email must be between 3 and 20 characters"
+    elif not valid_email(email):
+        email_invalid = "email is not valid. Please reenter"
+
+
+    return render_template('input.html', username_invalid=username_invalid, password_invalid=password_invalid, email_invalid=email_invalid)
+
+app.run()
